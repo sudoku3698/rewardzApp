d1d2de53038d6688783055bb45fd7128ad000dd1
Sudesh <sud.jad3698@gmail.com>
2018-06-22 16:44:02 +0530

    vue js added

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,6 +9,10 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+window.VueRouter = require('vue-router');
+
+Vue.use(VueRouter)
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -17,6 +21,23 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/hello',
+            name: 'hello',
+            component: Hello,
+        },
+    ],
+});
+
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router
 });
